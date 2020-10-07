5bafd752612d255af56f913cfd9cf170ad0a5fe6
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-06 21:20:06 -0400

    Added better nav support

--- real-estate-club-angular/src/app/app.component.ts
+++ real-estate-club-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'real-estate-club-angular';
+  @ViewChild('mobileMenu') mobileMenu: ElementRef
+  isDesktop: Boolean;
+  isMobileMenuOpen = false;
+
+  constructor() {
+    if(window.innerWidth < 1450){
+      this.isDesktop = false;
+    } else {
+      this.isDesktop = true;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if(window.innerWidth < 1450){
+      this.isDesktop = false;
+    } else {
+      this.isDesktop = true;
+    }
+  }
+
+  openMobileMenu(){
+    if(this.isMobileMenuOpen){
+      console.log('caught true')
+      this.mobileMenu.nativeElement.className = "bg-blue-300 text-white px-8 hidden";
+      this.isMobileMenuOpen = false;
+    } else{
+      console.log('caught false')
+      this.mobileMenu.nativeElement.className = "bg-blue-300 text-white px-8 block";
+      this.isMobileMenuOpen = true;
+    }
+  }
 
   toggle() {
     console.log("detected")
