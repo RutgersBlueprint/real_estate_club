e4aa133afc0f03cdd88a7a36f21b623ce83b842b
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-06 22:45:10 -0400

    Recolored and styled nav bar

--- real-estate-club-angular/src/app/app.component.ts
+++ real-estate-club-angular/src/app/app.component.ts
@@ -31,22 +31,12 @@ export class AppComponent {
   openMobileMenu(){
     if(this.isMobileMenuOpen){
       console.log('caught true')
-      this.mobileMenu.nativeElement.className = "bg-blue-300 text-white px-8 hidden";
+      this.mobileMenu.nativeElement.className = "hidden";
       this.isMobileMenuOpen = false;
     } else{
       console.log('caught false')
-      this.mobileMenu.nativeElement.className = "bg-blue-300 text-white px-8 block";
+      this.mobileMenu.nativeElement.className = "block";
       this.isMobileMenuOpen = true;
-    }
-  }
-
-  toggle() {
-    console.log("detected")
-    const elem = document.getElementById("main-menu");
-    if (elem.classList.contains("expanded")) {
-      elem.classList.remove("expanded");
-    } else {
-      elem.classList.add("expanded");
     }
   }
 }
