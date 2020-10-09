b6bb9956b4052fc8925d153e8d2b0c1c7e7ec5ad
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-09 17:25:58 -0400

    Added more assets into executives tiles

--- real-estate-club-angular/src/app/components/executive-tile/executive-tile.component.ts
+++ real-estate-club-angular/src/app/components/executive-tile/executive-tile.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit, Input } from "@angular/core";
 export class ExcutiveTileComponent implements OnInit {
   constructor() {}
 
-  @Input() label: String;
-  @Input() image: String;
+  @Input() description: string;
+  @Input() headline: string;
+  @Input() label: string;
+  @Input() image: string;
   ngOnInit() {}
 }
