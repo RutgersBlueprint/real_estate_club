60dac177041164f2843f7e40338d737ce912f2d6
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-12 23:39:07 -0400

    Working on adding a contact us page

--- real-estate-club-angular/src/app/pages/contact/contact.component.ts
+++ real-estate-club-angular/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebookF, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  faInstragram = faInstagram;
+  faFacebookF = faFacebookF;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
   constructor() { }
 
   ngOnInit() {
