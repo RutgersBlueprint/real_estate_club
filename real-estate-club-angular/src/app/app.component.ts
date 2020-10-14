55c2c927069b13ad14246f8b556ca4c925dfe81a
Xavier La Rosa <38988734+XavierLaRosa@users.noreply.github.com>
2020-10-13 22:09:20 -0400

    Working on adding dynamic color for nav bar

--- real-estate-club-angular/src/app/app.component.ts
+++ real-estate-club-angular/src/app/app.component.ts
@@ -39,4 +39,102 @@ export class AppComponent {
       this.isMobileMenuOpen = true;
     }
   }
+
+  @ViewChild('home', {static: true}) private home: ElementRef<HTMLDivElement>;
+  @ViewChild('homeButtonDT') homeButtonDT: ElementRef;
+  @ViewChild('about', {static: false}) private about: ElementRef<HTMLDivElement>;
+  @ViewChild('aboutButtonDT') aboutButtonDT: ElementRef;
+  @ViewChild('executives', {static: false}) private executives: ElementRef<HTMLDivElement>;
+  @ViewChild('executivesButtonDT') executivesButtonDT: ElementRef;
+  @ViewChild('alumni', {static: false}) private alumni: ElementRef<HTMLDivElement>;
+  @ViewChild('alumniButtonDT') alumniButtonDT: ElementRef;
+  @ViewChild('events', {static: false}) private events: ElementRef<HTMLDivElement>;
+  @ViewChild('eventsButtonDT') eventsButtonDT: ElementRef;
+  @ViewChild('contact', {static: false}) private contact: ElementRef<HTMLDivElement>;
+  @ViewChild('contactButtonDT') contactButtonDT: ElementRef;
+  @HostListener('window:scroll', ['$event'])
+  isScrolledIntoView(){
+    var page;
+    var button;
+    if(this.home){
+      page = this.home;
+      button = this.homeButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+    if(this.about){
+      page = this.about;
+      button = this.aboutButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+    if(this.executives) {
+      page = this.executives;
+      button = this.executivesButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+    if(this.alumni) {
+      page = this.alumni;
+      button = this.alumniButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+    if(this.events) {
+      page = this.events;
+      button = this.eventsButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+    if(this.contact) {
+      page = this.contact;
+      button = this.contactButtonDT;
+      const rect =page.nativeElement.getBoundingClientRect();
+      const topShown = rect.top >= 0;
+      const bottomShown = rect.bottom <= window.innerHeight;
+      if(topShown && bottomShown){
+        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
+      } else {
+        let button = this.executivesButtonDT;
+        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
+      }
+    }
+
+     
+  }
 }
