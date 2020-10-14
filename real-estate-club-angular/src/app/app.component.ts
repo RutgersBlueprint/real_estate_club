a158968e9460d40ddf973fc4a5b51b2d26d93f3c
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-10-14 01:33:55 -0400

    Added dynamic current nav highlight feature

--- real-estate-club-angular/src/app/app.component.ts
+++ real-estate-club-angular/src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'real-estate-club-angular';
   @ViewChild('mobileMenu') mobileMenu: ElementRef
+  @ViewChildren('home, about, executives, alumni, events, contact') sections: QueryList<ElementRef>; 
+  @ViewChildren('homeButtonDT, aboutButtonDT, executivesButtonDT, alumniButtonDT, eventsButtonDT, contactButtonDT') buttons: QueryList<ElementRef>; 
+  @ViewChild('homeButtonDT') homeButtonDT: ElementRef;
+  @ViewChild('aboutButtonDT') aboutButtonDT: ElementRef;
+  @ViewChild('executivesButtonDT') executivesButtonDT: ElementRef;
+  @ViewChild('alumniButtonDT') alumniButtonDT: ElementRef;
+  @ViewChild('eventsButtonDT') eventsButtonDT: ElementRef;
+  @ViewChild('contactButtonDT') contactButtonDT: ElementRef;
   isDesktop: Boolean;
   isMobileMenuOpen = false;
 
-  constructor() {
+  constructor() { // detect screen size
     if(window.innerWidth < 1450){
       this.isDesktop = false;
     } else {
@@ -20,7 +28,7 @@ export class AppComponent {
   }
 
   @HostListener('window:resize', ['$event'])
-  onResize(event) {
+  onResize(event) { // detect screen size change
     if(window.innerWidth < 1450){
       this.isDesktop = false;
     } else {
@@ -28,113 +36,55 @@ export class AppComponent {
     }
   }
 
-  openMobileMenu(){
+  openMobileMenu() { // show mobile or desktop nav
     if(this.isMobileMenuOpen){
-      console.log('caught true')
       this.mobileMenu.nativeElement.className = "hidden";
       this.isMobileMenuOpen = false;
     } else{
-      console.log('caught false')
       this.mobileMenu.nativeElement.className = "block";
       this.isMobileMenuOpen = true;
     }
   }
 
-  @ViewChild('home', {static: true}) private home: ElementRef<HTMLDivElement>;
-  @ViewChild('homeButtonDT') homeButtonDT: ElementRef;
-  @ViewChild('about', {static: false}) private about: ElementRef<HTMLDivElement>;
-  @ViewChild('aboutButtonDT') aboutButtonDT: ElementRef;
-  @ViewChild('executives', {static: false}) private executives: ElementRef<HTMLDivElement>;
-  @ViewChild('executivesButtonDT') executivesButtonDT: ElementRef;
-  @ViewChild('alumni', {static: false}) private alumni: ElementRef<HTMLDivElement>;
-  @ViewChild('alumniButtonDT') alumniButtonDT: ElementRef;
-  @ViewChild('events', {static: false}) private events: ElementRef<HTMLDivElement>;
-  @ViewChild('eventsButtonDT') eventsButtonDT: ElementRef;
-  @ViewChild('contact', {static: false}) private contact: ElementRef<HTMLDivElement>;
-  @ViewChild('contactButtonDT') contactButtonDT: ElementRef;
-  @HostListener('window:scroll', ['$event'])
-  isScrolledIntoView(){
-    var page;
-    var button;
-    if(this.home){
-      page = this.home;
-      button = this.homeButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
-    if(this.about){
-      page = this.about;
-      button = this.aboutButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
-    if(this.executives) {
-      page = this.executives;
-      button = this.executivesButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
-    if(this.alumni) {
-      page = this.alumni;
-      button = this.alumniButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
-    if(this.events) {
-      page = this.events;
-      button = this.eventsButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
-    if(this.contact) {
-      page = this.contact;
-      button = this.contactButtonDT;
-      const rect =page.nativeElement.getBoundingClientRect();
-      const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
-      if(topShown && bottomShown){
-        button.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase"
-      } else {
-        let button = this.executivesButtonDT;
-        button.nativeElement.className = "bg-clear p-3 rounded-lg shadow-none uppercase"
-      }
-    }
+  isElementInViewport(el): boolean {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.bottom >= 500 &&
+      rect.right >= 0 &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
-     
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event) {
+    if(this.isDesktop){
+      const activeSection = this.sections.toArray().findIndex(section => this.isElementInViewport(section.nativeElement));
+      const areas = 'home, about, executives, alumni, events, contact';
+      const sectionName = areas.split(', ')[activeSection];
+      this.buttons.toArray().forEach(b =>{
+        b.nativeElement.className = "bg-clear p-3 rounded-lg uppercase";
+      })
+      switch(sectionName){
+        case 'home':
+          this.homeButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+        case 'about':
+          this.aboutButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+        case 'executives':
+          this.executivesButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+        case 'alumni':
+          this.alumniButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+        case 'events':
+          this.eventsButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+        case 'contact':
+          this.contactButtonDT.nativeElement.className = "bg-red-700 p-3 rounded-lg shadow uppercase";
+          break;
+      }
+    }
   }
 }
